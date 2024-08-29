5b338df3f91728c7b7229ebfe5993aa36736ab6f
Kevin Ranpura <85952778+kevin1043@users.noreply.github.com>
2024-08-29 18:38:51 +0530

    Update streamlit_app.py
    Updated search functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("Name on the Smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
 ingredient_list = st.multiselect(
     'Choose up to 5 ingredients: ',
     my_dataframe,
@@ -20,6 +21,8 @@ ingredient_list = st.multiselect(
 )
 if ingredient_list:
     ingredients_string = ''
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrients Information')
