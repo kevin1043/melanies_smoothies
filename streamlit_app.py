d7569a267676a405ac6cf243d5d80363f9125a63
Kevin Ranpura <85952778+kevin1043@users.noreply.github.com>
2024-08-29 18:12:53 +0530

    Update streamlit_app.py
    Added fruits list

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,4 @@ if ingredient_list:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
